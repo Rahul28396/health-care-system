487bf5df9ad87ba300f01ddd9eab3cbbb60e84d8
RahulMondal28396 <rahul.mondal28396@gmail.com>
2024-01-03 21:34:16 +0530

    Added storage service

--- src/app/appointments/appointments.service.ts
+++ src/app/appointments/appointments.service.ts
@@ -1,5 +1,7 @@
-import { Injectable } from '@angular/core';
+import { Injectable, inject } from '@angular/core';
 import { Appointment } from '../models/appointments/appointment.model';
+import { StorageService } from '../services/storage.service';
+import { STORAGE_TYPE } from '../utils/enums';
 
 @Injectable({
   providedIn: 'root'
@@ -9,17 +11,18 @@ export class AppointmentsService {
   appointments: Array<Appointment> = [];
 
   totalAppointments = this.appointments.length;
-  
+
+  storageService= inject(StorageService);
+
 
   loadAppointments(start: number, count: number): Array<Appointment> {
+    this.appointments = this.storageService.getAllValueOf<Appointment>(STORAGE_TYPE.APPOINTMENT);
     const appointments = [...this.appointments];
     return appointments.splice(start, count);
   }
 
   createAppointment(newAppointment: Appointment): void {
     newAppointment.id = this.totalAppointments + 1;
-    debugger;
-    this.appointments.push(newAppointment);
-    console.log(this.appointments.length);
+    this.storageService.addValueTo<Appointment>(STORAGE_TYPE.APPOINTMENT,newAppointment);
   }
 }
---
+++ src/app/services/storage.service.ts
@@ -0,0 +1,34 @@
+import { Injectable } from '@angular/core'
+
+@Injectable({
+  providedIn: 'root'
+})
+export class StorageService {
+
+  constructor() { }
+
+  getAllValueOf<T>(storageKey: string): Array<T>{
+    const list = localStorage.getItem(storageKey);
+    if(typeof list === 'string')
+      return JSON.parse(list);
+    return [];
+  }
+
+  addValueTo<T>(storageKey: string, value:T): T {
+    const data = this.getAllValueOf(storageKey);
+    data.push(value);
+    localStorage.setItem(storageKey,JSON.stringify(data));
+    return value;
+  }
+
+  updateValueOf<T>(storageKey: string, value:T): T{
+    const data = this.getAllValueOf(storageKey);
+    data.push(value);
+    localStorage.setItem(storageKey,JSON.stringify(data));
+    return value;
+  }
+
+  deleteValueOf<T>(storageKey: string, value: Array<T>): void{
+    localStorage.setItem(JSON.stringify(value),storageKey);
+  }
+}
