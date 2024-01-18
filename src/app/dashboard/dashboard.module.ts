93c9023c0371a34a6626f2894aa97c64b57f6e03
RahulMondal28396 <rahul.mondal28396@gmail.com>
2024-01-19 01:26:29 +0530

    Added dashboard chart

--- src/app/dashboard/dashboard.module.ts
+++ src/app/dashboard/dashboard.module.ts
@@ -2,14 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { DashboardRoutingModule } from './dashboard-routing.module';
-
+import { CanvasJSAngularChartsModule } from '@canvasjs/angular-charts';
 @NgModule({
   declarations: [
     DashboardComponent
   ],
   imports: [
     CommonModule,
-    DashboardRoutingModule
+    DashboardRoutingModule,
+    CanvasJSAngularChartsModule
   ]
 })
 export class DashboardModule { }
--- src/app/dashboard/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard/dashboard.component.ts
@@ -7,4 +7,19 @@ import { Component } from '@angular/core';
 })
 export class DashboardComponent {
   appointments = new Array(10).fill(1);
+  chartOptions = {
+    title: {
+    	text: "Basic Column Chart in Angular"
+    },
+    data: [{
+      type: "column",
+      dataPoints: [
+        { label: "Apple",  y: 10  },
+        { label: "Orange", y: 15  },
+        { label: "Banana", y: 25  },
+        { label: "Mango",  y: 30  },
+        { label: "Grape",  y: 28  }
+      ]
+    }]                
+  };
 }
